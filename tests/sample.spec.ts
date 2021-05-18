006f6d7f5679ad87315c820ac6af31c9957e3420
geborskimateusz <mateuszgeborski@gmail.com>
2021-05-18 06:19:27 +0200

    closes #3

--- tests/sample.spec.ts
+++ tests/sample.spec.ts
@@ -3,16 +3,12 @@ import chaiHttp from 'chai-http';
 import chai from 'chai';
 import express = require("express");
 import { app } from "../src/app";
+import { getRepository, Repository } from 'typeorm';
+import MovieEntity from '../src/entities/movie';
 
 chai.use(chaiHttp);
 
 const createFakeServer = async () => {
-    //     const application = app();
-    //     await application. 
-    
-    
-    //     return app
-    
     return app().then(server => {
         const apiPort: number = 30001;
         server.listen(apiPort);
@@ -23,22 +19,37 @@ const createFakeServer = async () => {
 describe('API', () => {
     describe('GET', () => {
         let fakeServer: express.Application;
+        let movieRespository: Repository<MovieEntity>;
+
+        before(async () => {
+            fakeServer = await createFakeServer();
+            movieRespository = getRepository(MovieEntity);
+            console.log("BEFORE HOOK")
+        })
 
         beforeEach(async () => {
-            fakeServer = await createFakeServer();
+            console.log("BEFORE EACH HOOK")
         })
 
 
         describe('/GET book', () => {
             it('it should GET all the books', (done) => {
                 chai.request(fakeServer)
-                    .get('/api/findById')
-                    .end((err, res) => {
+                    .post('/api/create')
+                    .send({ title: "some test title here" })
+                    .end(async (err, res) => {
                         expect(res).to.have.status(200);
+                        console.log(res.body)
+                        expect(res.body).to.have.property("payload");
+                        expect(res.body).to.have.property("type");
 
-                        const expected = JSON.stringify({ msg: 'Found user witha id 1234' });
-                        const body = JSON.stringify(res.body);
-                        expect(body).to.equal(expected)
+                        const movie = res.body.payload.movie;
+                        expect(movie).to.have.property("id").and.is.not.empty;
+
+                        const found = await movieRespository.findOne(movie.id);
+                        console.log(found)
+
+
 
                         done();
                     });
